1dd83cce9310cd2e925da29315a740d11e04d774
Khanza Gusung <wangusung46@gmail.com>
2022-03-28 00:12:48 +0700

    Fix

--- src/main/java/com/rayhan/alpc/AlpcApplication.java
+++ src/main/java/com/rayhan/alpc/AlpcApplication.java
@@ -2,8 +2,11 @@ package com.rayhan.alpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AlpcApplication {
@@ -17,6 +20,16 @@ public class AlpcApplication {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("Regis");
         return modelAndView;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
