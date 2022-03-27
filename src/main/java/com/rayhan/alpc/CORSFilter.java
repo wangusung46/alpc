ca206caa22404a199f12ba12de01351723cfa484
Khanza Gusung <wangusung46@gmail.com>
2022-03-27 23:33:06 +0700

    Fix

--- src/main/java/com/rayhan/alpc/CORSFilter.java
+++ src/main/java/com/rayhan/alpc/CORSFilter.java
@@ -31,8 +31,8 @@ public class CORSFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-//        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-//        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
