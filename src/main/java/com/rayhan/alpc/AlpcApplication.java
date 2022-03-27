62b91ef66f7b9efe287a87cbe795ca501bcd686d
Khanza Gusung <wangusung46@gmail.com>
2022-03-27 23:01:58 +0700

    Fix

--- src/main/java/com/rayhan/alpc/AlpcApplication.java
+++ src/main/java/com/rayhan/alpc/AlpcApplication.java
@@ -2,12 +2,21 @@ package com.rayhan.alpc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @SpringBootApplication
 public class AlpcApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AlpcApplication.class, args);
+    }
+
+    @RequestMapping("/regis")
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("Regis");
+        return modelAndView;
     }
 
 }
